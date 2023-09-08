2eac3f0ab7aadc85b692c456a6aba8f931c95eca
Keerthana S <104012061+keerthana331@users.noreply.github.com>
2023-09-08 10:58:08 +0530

    Update message.py

--- message.py
+++ message.py
@@ -11,7 +11,6 @@ options.add_argument("--user-data-dir=/home/username/.config/google-chrome")
 
 driver = webdriver.Chrome(options=options)  
 
-image_path="C:\KEERTHANA.S_Resume.pdf"
 
 driver.get('https://web.whatsapp.com')
 input("Press ENTER...")
@@ -27,13 +26,8 @@ with open('numbers.txt', 'r') as file:
         num = n.rstrip()
         link = f'https://web.whatsapp.com/send/?phone={num}'
         driver.get(link)
-        time.sleep(10)
-        actions = ActionChains(driver)
-        attach_btn= driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div')
-        attach_btn.click()
-        image_btn= driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[1]/li/div/input')
-        image_btn.send_keys(image_path)
         time.sleep(5)
+        actions = ActionChains(driver)
         for line in msg.split('\n'):
             actions.send_keys(line)
             actions.key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT)
